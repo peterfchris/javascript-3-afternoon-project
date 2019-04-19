b19beca5c371b8bd12df0950ff647ea9487463e6
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-04-19 17:47:28 -0600

    finished higher order problems

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(element, index) {
+  if (element % 2 === 0) 
+  return element
+})
+  
 
 
 
@@ -44,7 +48,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index) {
+    return element * 1.07
+}); 
 
 
 
@@ -63,7 +69,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(runningTotal, curElement, curIndex) {
+  return runningTotal + curElement
+})
 
 
 
@@ -88,8 +96,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-//Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+// Code Here
+var myStrongest = monstersInYourPocket.filter(function(element, index){
+  //  monstersInYourPocket[element].CP > 200
+  return element.CP > 200
+ });
 
 
 
@@ -103,10 +114,15 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax. 
+  Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(element, index){
+  var taxTotal = element.price * element.tax
+  let total = taxTotal + element.price 
+  return total
+})
 
 
 
@@ -126,6 +142,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(function(element, index){
+  element.owner["Bob"] 
+})
 
 
